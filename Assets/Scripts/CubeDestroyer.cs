43ff9b8506a65a7ca6068b0ad7f77f9efa7985e1
Eldar Kurbanov <EldarK@outlook.com>
2021-02-22 22:30:03 +0300

    Complete task

---
+++ Assets/Scripts/CubeDestroyer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDestroyer : MonoBehaviour
+{
+    
+    public GameObject cubeManager;
+    private CubeSpawner _cubeSpawner;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cubeSpawner = cubeManager.GetComponent<CubeSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        Destroy(gameObject);
+        _cubeSpawner.incrementCubeCounter();
+    }
+}
--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeSpawner : MonoBehaviour
 {
@@ -8,30 +9,37 @@ public class CubeSpawner : MonoBehaviour
     public GameObject exampleCube, children;
     private float xMin, xMax, zMin, zMax;
     public int cubeLimit = 10;
-    private int cubeCounter;
+    private int destroyedCubeCounter = 0;
+    public Text scoreShower;
     
+    public void incrementCubeCounter()
+    {
+        destroyedCubeCounter++;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("spawnNewCube", 1f, 1f);
-        xMax = plane.transform.position.x + plane.transform.localScale.x / 2;
+        xMax = plane.transform.position.x + plane.transform.lossyScale.x * 2 + plane.transform.localScale.x * 2;
         xMin = -xMax;
-        zMax = plane.transform.position.z + plane.transform.localScale.z / 2;
+        zMax = plane.transform.position.z + plane.transform.lossyScale.z * 2 + plane.transform.localScale.z * 2;
         zMin = -zMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreShower.text = destroyedCubeCounter.ToString();
     }
 
     void spawnNewCube()
     {
+        var cubeCounter = children.transform.childCount;
         if (cubeCounter < cubeLimit)
         {
             var newCube = Instantiate(exampleCube);
+            newCube.SetActive(true);
             newCube.transform.position = new Vector3(Random.Range(xMin, xMax), exampleCube.transform.position.y, Random.Range(zMin, zMax));
             newCube.transform.parent = children.transform;
             cubeCounter++;
